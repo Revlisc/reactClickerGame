191bb20d0656fcb2b527a62cc8d064f61706667b
Dale <dalesimmons0210@gmail.com>
2021-08-05 08:09:51 -0700

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Clicker from './Components/Clicker';
+
+function App() {
+  return (
+    <div>
+      <Clicker/>
+    </div>
+  );
+}
+
+export default App;
